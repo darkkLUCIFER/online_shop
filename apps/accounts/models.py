83e9b1cecd07cfb66ef2e30c990f045b932b46cb
sorena <mdn1376@gmail.com>
2024-07-23 13:22:48 +0330

    feat: made custom User & custom UserManager

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
-# Create your models here.
+from apps.accounts.managers import UserManager
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True, verbose_name="Email")
+    phone_number = models.CharField(max_length=11, unique=True, verbose_name="Phone Number")
+    full_name = models.CharField(max_length=255, verbose_name="Full Name")
+    is_active = models.BooleanField(default=True, verbose_name="Active")
+    is_admin = models.BooleanField(default=False, verbose_name="Admin")
+
+    USERNAME_FIELD = 'phone_number'  # field for authenticate users
+    REQUIRED_FIELDS = ['email', 'full_name']  # used just in createsuperuser command
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
