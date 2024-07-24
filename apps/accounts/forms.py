c058a4b77ecdff1a64eaf229f1b15a02191192c3
sorena <mdn1376@gmail.com>
2024-07-24 12:40:40 +0330

    fix: remove / from help_text
    removed / from help_text in UserChangeForm

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -42,7 +42,7 @@ class UserChangeForm(forms.ModelForm):
     """
 
     password = ReadOnlyPasswordHashField(
-        help_text="you can change password using <a href='../password/'>this form</a>/")
+        help_text="you can change password using <a href='../password/'>this form</a>.")
 
     class Meta:
         model = User
