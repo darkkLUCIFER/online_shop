81a0adfc3ef02c12e01a0cff99079106f27446f8
sorena <mdn1376@gmail.com>
2024-07-28 23:19:47 +0330

    feat: added model Category & Product
    added models and register in admin
    
    also added pillow for work with images

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.products.models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'active',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'available')
+    prepopulated_fields = {'slug': ('name',)}
--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+from apps.utils.base_model import BaseModel
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=200, verbose_name='Name')
+    slug = models.SlugField(max_length=200, unique=True, verbose_name='Slug')
+    active = models.BooleanField(default=True, verbose_name='Active')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        db_table = 'category'
+        ordering = ['name']
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+
+class Product(BaseModel):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', verbose_name='Category')
+    name = models.CharField(max_length=200, verbose_name='Name')
+    slug = models.SlugField(max_length=200, unique=True, verbose_name='Slug')
+    image = models.ImageField(upload_to='products/%Y/%m/%d/', verbose_name='Image')
+    description = models.TextField(verbose_name='Description', blank=True, null=True)
+    price = models.IntegerField(verbose_name='Price')
+    available = models.BooleanField(verbose_name='Available', default=True)
+
+    def __str__(self):
+        return f'{self.name} - {self.category}'
+
+    class Meta:
+        db_table = 'product'
+        ordering = ['name']
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
