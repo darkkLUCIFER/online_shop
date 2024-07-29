cf80beecb2f7595b99dd0100aaf886d35b0499f6
sorena <mdn1376@gmail.com>
2024-07-29 17:21:56 +0330

    feat: added method get_objects to class Bucket

--- apps/utils/bucket.py
+++ apps/utils/bucket.py
@@ -6,6 +6,9 @@ class Bucket:
     """CDN Bucket manager
 
     init method created connection
+
+    NOTE:
+        none of these methods are async. use public interface in tasks.py modules instead.
     """
 
     def __init__(self):
@@ -14,5 +17,13 @@ class Bucket:
             service_name=settings.AWS_SERVICE_NAME,
             aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
             aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-            endpoint_url=settings.AWS_ENDPOINT_URL,
+            endpoint_url=settings.AWS_S3_ENDPOINT_URL,
         )
+
+    @staticmethod
+    def get_instance():
+        return Bucket()
+
+    def get_objects(self):
+        result = self.connection.list_objects_v2(Bucket=settings.AWS_STORAGE_BUCKET_NAME)
+        return result
