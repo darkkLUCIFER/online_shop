91da58ba9f1ac49e27ee2edcff2b9ebc92c49cb0
sorena <mdn1376@gmail.com>
2024-07-29 08:53:58 +0330

    feat: added products to HomeView

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.views import View
 
+from apps.products.models import Product
+
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'home/home.html')
+        products = Product.objects.filter(available=True)
+        context = {
+            'products': products
+        }
+        return render(request, 'home/home.html', context)
