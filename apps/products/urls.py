ba381d91f20caa7233ef3533948daa82cf7d3861
sorena <mdn1376@gmail.com>
2024-07-29 08:55:13 +0330

    feat: made ProductDetailView

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from apps.products import views
+
 app_name = 'products'
 
 urlpatterns = [
-
+    path('<slug:slug>/', views.ProductDetailView.as_view(), name='product_detail'),
 ]
--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import View
 
-# Create your views here.
+from apps.products.models import Product
+
+
+class ProductDetailView(View):
+    form_class = 'products/detail.html'
+
+    def get(self, request, slug):
+        product = get_object_or_404(Product, slug=slug)
+        context = {
+            'product': product
+        }
+        return render(request, self.form_class, context)
