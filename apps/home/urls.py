24fdb92db7c467154fbcd7c7e9a220b2b2571bdf
sorena <mdn1376@gmail.com>
2024-07-29 17:23:41 +0330

    feat: made BucketHomeView

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -6,4 +6,5 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('bucket/', views.BucketHomeView.as_view(), name='bucket'),
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 
+from apps.home.tasks import get_all_bucket_objects_task
 from apps.products.models import Product
 
 
@@ -11,3 +12,14 @@ class HomeView(View):
             'products': products
         }
         return render(request, 'home/home.html', context)
+
+
+class BucketHomeView(View):
+    template_name = 'home/bucket.html'
+
+    def get(self, request):
+        objects = get_all_bucket_objects_task()
+        context = {
+            'objects': objects
+        }
+        return render(request, self.template_name, context)
