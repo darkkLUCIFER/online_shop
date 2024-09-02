7cd56c76a0c309df5a91f56776847f1a5f89fe7a
sorena <mdn1376@gmail.com>
2024-09-02 19:33:08 +0330

    fix: added otp_code parameter in send_otp_code_task

--- apps/accounts/tasks.py
+++ apps/accounts/tasks.py
@@ -4,5 +4,5 @@ from apps.accounts.services import KavenegarService
 
 
 @shared_task
-def send_otp_code_task(phone_number):
-    KavenegarService.get_instance().send_otp_code(phone_number)
+def send_otp_code_task(phone_number, otp_code):
+    KavenegarService.get_instance().send_otp_code(phone_number, otp_code)
