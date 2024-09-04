cb9c27966ceaebcd830d6b806bb60e4bab0511e7
sorena <mdn1376@gmail.com>
2024-09-04 12:02:36 +0330

    feat: made remove_expired_otp_codes_task

--- apps/accounts/tasks.py
+++ apps/accounts/tasks.py
@@ -1,8 +1,19 @@
-from celery import shared_task
+from datetime import timedelta
 
+from celery import shared_task
+from django.utils import timezone
+
+from apps.accounts.models import OtpCode
 from apps.accounts.services import KavenegarService
 
 
 @shared_task
 def send_otp_code_task(phone_number, otp_code):
     KavenegarService.get_instance().send_otp_code(phone_number, otp_code)
+
+
+@shared_task
+def remove_expired_otp_codes_task():
+    expired_time = timezone.now() - timedelta(minutes=2)
+
+    OtpCode.objects.filter(created_at__lt=expired_time).delete()
